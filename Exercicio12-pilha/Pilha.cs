b45fb42132b5cd4c6461641d940b9f07e6516beb
Daniel Santos De Sousa <64164438+DaniFTT@users.noreply.github.com>
2021-03-10 17:15:12 -0300

    added exercise 12 not completed

---
+++ Exercicio12-pilha/Pilha.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio12_pilha
+{
+    class Pilha
+    {
+        private readonly int capacidade = 10;
+        private string[] dados;
+        private int topo = -1;
+
+        public Pilha() 
+        {
+            dados = new string[capacidade];
+        }
+
+        public Pilha(int capacidade)
+        {
+            this.capacidade = capacidade;
+            dados = new string[capacidade];
+        }
+
+
+        public int Tamanho()
+        {
+            return topo + 1;
+        }
+
+        // este método empilha um valor string na pilha
+        public void Empilha(string p_valor, int posicao)
+        {
+            if (Tamanho() != capacidade)
+            {
+                topo++;
+                dados[topo] = p_valor;
+            }
+            else
+            {
+                throw new Exception("A pilha está cheia!!!");
+            }
+        }
+
+        // este método desempilha um valor da pilha
+        public string Desempilha()
+        {
+            if (Tamanho() == 0)
+            {
+                throw new Exception("A pilha está vazia!!!");
+            }
+            else
+            {
+                string r = dados[topo];
+                dados[topo--] = null;
+                return r;
+            }
+        }
+
+        // este método devolve o valor que está no topo
+        public string RetornaTopo()
+        {
+            if (Tamanho() == 0)
+                throw new Exception("A pilha está vazia!!!");
+            else
+                return dados[topo];
+        }
+    }
+}
---
+++ Exercicio12-pilha/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio12_pilha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao = 0;
+            Pilha minhaPilha = new Pilha(15);
+
+            Console.WriteLine();
+            do
+            {
+                try
+                {
+                    Console.Write("Digite: 1->Empilha 2->Desempilha " +
+                    "3->Topo 4->tamanho 5->Listar 9->Sair: ");
+                    opcao = Convert.ToInt32(Console.ReadLine());
+                    if (opcao == 1)
+                    {
+                        Console.Write("\nDigite um dado para empilhar: ");
+                        string valor = Console.ReadLine();
+
+                        Console.Write($"\nDigite a posição onde quer empilhar (0 até {minhaPilha.Tamanho()}) ");
+                        int posicao = Convert.ToInt32(Console.ReadLine());
+
+                        minhaPilha.Empilha(valor, posicao);
+                        Console.WriteLine();
+                    }
+                    else if (opcao == 2)
+                    {
+                        string desempilhado = minhaPilha.Desempilha();
+                        Console.WriteLine($"\nDesempilhado: {desempilhado}");
+
+                        Console.WriteLine();
+                    }
+                    else if (opcao == 3)
+                    {
+                        Console.WriteLine("\nTopo: {0}",
+                            minhaPilha.RetornaTopo());
+
+                        Console.WriteLine();
+                    }
+                    else if (opcao == 4)
+                    {
+                        Console.WriteLine("\nTamanho da pilha: {0}", minhaPilha.Tamanho());
+
+                        Console.WriteLine();
+                    }
+                    //else if (opcao == 5)
+                    //{
+                    //    Console.WriteLine("\nListagem da pilha");
+                    //    Pilha pilhaAuxiliar = new Pilha();
+
+                    //    while (minhaPilha.Tamanho() > 0)
+                    //    {
+                    //        string aux = minhaPilha.Desempilha();
+
+                    //        Console.WriteLine(aux);
+                    //        pilhaAuxiliar.Empilha(aux);
+                    //    }
+
+                    //    while (pilhaAuxiliar.Tamanho() > 0)
+                    //        minhaPilha.Empilha(pilhaAuxiliar.Desempilha());
+
+                    //    Console.WriteLine();
+                    //}
+                    else if (opcao == 9)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+                catch (Exception erro)
+                {
+                    Console.WriteLine("\nErro! " + erro.Message + "\n");
+                }
+            }
+            while (opcao != 9);
+        }
+    
+    }
+}
